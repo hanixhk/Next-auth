ef219c413a07b7b7213a905b5c33c871c25324a6
hanixhk <abdulhannanxhk@gmail.com>
2024-08-14 22:58:35 +0500

    mongoose file updated

--- src/lib/mongodb/mongoose.ts
+++ src/lib/mongodb/mongoose.ts
@@ -1,34 +1,23 @@
 import mongoose from 'mongoose';
 
-// Flag to check if the connection is already initialized
 let initialized = false;
 
-// Define the async function to connect to MongoDB
 export const connect = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
-    if (initialized) {
-        console.log('already connected');
-        return;
-    }
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
 
-    const mongoUri = process.env.MONGODB_URI;
-
-    if (!mongoUri) {
-        console.error('Error: MONGODB_URI environment variable is not defined.');
-        return;
-    }
-
-    try {
-        // Attempt to connect to MongoDB without deprecated options
-        await mongoose.connect(mongoUri, {
-            dbName: "Next-Auth"
-        });
-        console.log('connected to mongodb');
-        initialized = true;
-
-    } catch (error) {
-        // Log any errors that occur during connection
-        console.error("mongodb error", error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      dbName: 'Auth-app',
+      
+    });
+    console.log('MongoDB connected');
+    initialized = true;
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
