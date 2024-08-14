dc285a2eae0b8cca4f7cd20f609421b8d3cb5e4b
hanixhk <abdulhannanxhk@gmail.com>
2024-08-14 22:27:51 +0500

    Add mongodb  and mongoose to save user data from clerk

---
+++ src/lib/mongodb/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+// Flag to check if the connection is already initialized
+let initialized = false;
+
+// Define the async function to connect to MongoDB
+export const connect = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (initialized) {
+        console.log('already connected');
+        return;
+    }
+
+    const mongoUri = process.env.MONGODB_URI;
+
+    if (!mongoUri) {
+        console.error('Error: MONGODB_URI environment variable is not defined.');
+        return;
+    }
+
+    try {
+        // Attempt to connect to MongoDB without deprecated options
+        await mongoose.connect(mongoUri, {
+            dbName: "NextAuth"
+        });
+        console.log('connected to mongodb');
+        initialized = true;
+
+    } catch (error) {
+        // Log any errors that occur during connection
+        console.error("mongodb error", error);
+    }
+}
